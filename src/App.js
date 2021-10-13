15cf41acca86fa37a083cffff5809fd3312f1c4a
Viraj Shinde <virajshinde48@gmail.com>
2021-10-13 18:46:50 +0530

    add local to git

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import './App.css';
+import useBookSearch from './useBookSearch';
+import React, { useState, useRef, useCallback } from 'react';
+
+function App() {
+  const [query, setQuery] = useState("");
+  const [pageNumber, setPageNumber] = useState(1);
+  const { books, hasMore, loading, error } = useBookSearch(query, pageNumber);
+  const observer = useRef()
+  const lastBookElementRef = useCallback(node => {
+    if (loading) return
+    if (observer.current) observer.current.disconnect()
+    observer.current = new IntersectionObserver(entries => {
+      if (entries[0].isIntersecting && hasMore) {
+        setPageNumber(prevPageNumber =>
+          prevPageNumber + 1
+        )
+        console.log("viosible")
+      }
+    })
+    if (node) observer.current.observe(node)
+    console.log(node)
+  }, [loading, hasMore])
+
+  function handleChange(e) {
+    setQuery(e.target.value)
+    setPageNumber(1)
+  }
+
+  return (
+    <div className="App">
+      <input type="text" value={query} onChange={handleChange}></input>
+      {books.map((book, index) => {
+        if (books.length === index + 1) {
+          return <div ref={lastBookElementRef} key={book}>{book}</div>
+        } else {
+          return <div key={book}>{book}</div>
+        }
+
+      })}
+
+      <div>{loading && "Loading...."}</div>
+      <div>{error && "Error"}</div>
+    </div>
+  );
+}
+
+export default App;
